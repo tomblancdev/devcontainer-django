3e81187ad31e18c25bfeded4548cb52799a659e0
Tom <dev@tomblanc.fr>
2024-01-26 16:31:16 +0000

    Improving config

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,5 +1,38 @@
-cd /workspace && \
-poetry install && \
-poetry run python manage.py migrate && \
-poetry run python manage.py createsuperuser --noinput && \
-bash /workspace/.devcontainer/scripts/install-pre-commit.sh
+cd /workspace
+
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo -e "${CYAN}Installing Poetry${NC}"
+poetry install
+
+echo -e "${CYAN}Making Migrations${NC}"
+poetry run python manage.py migrate
+
+echo -e "${CYAN}Creating Superuser${NC}"
+poetry run python manage.py createsuperuser --noinput
+
+echo -e """${CYAN}
+                       ____________________
+                     //|           |        |
+                   //  |           |          |
+      ___________//____|___________|__________()\__________________
+    /__________________|_=_________|_=___________|_________________{}
+    [           ______ |           | .           | ==  ______      { }
+  __[__        /##  ##\|           |             |    /##  ##\    _{# }_
+ {_____)______|##    ##|___________|_____________|___|##    ##|__(______}
+             /  ##__##                              /  ##__##        |
+----------------------------------------------------------------------------
+
+██     ██ ███████ ██       ██████  ██████  ███    ███ ███████     ██ ██ ██
+██     ██ ██      ██      ██      ██    ██ ████  ████ ██          ██ ██ ██
+██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████       ██ ██ ██
+██ ███ ██ ██      ██      ██      ██    ██ ██  ██  ██ ██
+ ███ ███  ███████ ███████  ██████  ██████  ██      ██ ███████     ██ ██ ██
+
+----------------------------------------------------------------------------
+
+${NC}
+"""
+
+echo -e "${CYAN}Welcome to the container !${NC}"
