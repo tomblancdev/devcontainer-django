aab6575e1fa0270c386c70a4126ba33a68d918ae
Tom <dev@tomblanc.fr>
2024-01-28 10:52:22 +0000

    Adding run-prod-script

--- .devcontainer/scripts/run-prod.sh
+++ .devcontainer/scripts/run-prod.sh
@@ -1,6 +1,7 @@
 cd /workspace
 
 CYAN='\033[0;36m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo -e "${CYAN}Installing Poetry${NC}"
@@ -12,9 +13,19 @@ poetry run python manage.py migrate
 echo -e "${CYAN}Creating Superuser${NC}"
 poetry run python manage.py createsuperuser --noinput
 
+echo -e "${CYAN}Collecting Static Files${NC}"
+poetry run python manage.py collectstatic --noinput
+
 echo -e "${CYAN}Copying Nginx Configuration${NC}"
 cp /workspace/.devcontainer/nginx.conf /etc/nginx/nginx.conf
 
 echo -e "${CYAN}Starting Nginx${NC}"
 service nginx stop
 service nginx start
+
+if $DEBUG; then
+    echo -e "${RED}RUNNING WITH DEBUG${NC}"
+fi
+
+echo -e "${CYAN}Starting Server${NC}"
+poetry run python manage.py runserver
